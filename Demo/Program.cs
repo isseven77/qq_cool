8983bcf4ad28e79691eb70504024e7e8a7318c2e
isseven77 <3335985539@qq.com>
2022-06-24 11:41:44 +0800

    eee

---
+++ Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+       
+    static void Main(string[] args)
+    {
+        using(Mutex mutex=new Mutex(false, "MutexDemo"))
+            {
+                if (!mutex.WaitOne(5000,false))
+                {
+                    Console.WriteLine("start");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("End");
+                Console.ReadKey();
+            }
+    }
+
+
+
+
+    }
+}
